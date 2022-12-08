b420e238bfe9125ca5ef70ceba26ca448fad8ea6
baiwusanyu-c <740132583@qq.com>
2022-12-08 17:44:19 +0800

    update: function.rs

--- src/data-type/function.rs
+++ src/data-type/function.rs
@@ -10,3 +10,10 @@ fn main() {
     let x = plus_five(5);
     println!("The value of x is: {}", x);
 }
+
+// 永不发散的函数
+// 当用 ! 作函数返回类型的时候，表示该函数永不返回( diverge function )，
+// 特别的，这种语法往往用做会导致程序崩溃的函数：
+// fn dead_end() -> ! {
+//   panic!("你已经到了穷途末路，崩溃吧！");
+// }
