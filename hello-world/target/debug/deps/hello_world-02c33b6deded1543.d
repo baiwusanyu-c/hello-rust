4238981b11ed09af44813983284108591ae04fc7
baiwusanyu-c <chenzhihui@chabaidao.com>
2024-09-20 17:10:36 +0800

    chore: 让我们重学 rust ！

---
+++ hello-world/target/debug/deps/hello_world-02c33b6deded1543.d
@@ -0,0 +1,5 @@
+D:\project-github\hello-rust\hello-world\target\debug\deps\hello_world-02c33b6deded1543.exe: src/main.rs
+
+D:\project-github\hello-rust\hello-world\target\debug\deps\hello_world-02c33b6deded1543.d: src/main.rs
+
+src/main.rs:
---
+++ hello-world/target/debug/hello-world.d
@@ -0,0 +1,1 @@
+D:\project-github\hello-rust\hello-world\target\debug\hello-world.exe: D:\project-github\hello-rust\hello-world\src\main.rs
